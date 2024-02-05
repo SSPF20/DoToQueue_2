0951cd2a001eb9510393484ef84c606335eacfc1
SSPF20 <simi.parraga@gmail.com>
2024-02-04 22:42:18 -0500

    Add router settings

--- ToDoQueue_2/src/routes/router.jsx
+++ ToDoQueue_2/src/routes/router.jsx
@@ -1,16 +1,36 @@
 //React libraries
 import React from 'react'
-import {ReactBrowserRouter} from 'react-router-dom'
+import {createBrowserRouter} from 'react-router-dom'
 
 //Pages
 import Home from '../pages/Home'
 import NotFound from '../pages/NotFound'
 import ToDoList from '../pages/ToDoList'
 
-const router = () => {
-  return (
-    <div>router</div>
-  )
-}
+//Base component
+import Layout from '../layout/Layout'
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Layout/>,
+        errorElement:<NotFound/>,
+        children:[
+            {
+                errorElement:<NotFound/>,
+                children:[
+                    {
+                        path:"/",
+                        element:<Home/>
+                    },
+                    {
+                        path:"/todo",
+                        element:<ToDoList/>
+                    },
+                ]
+            }
+        ]
+    }
+])
 
 export default router
